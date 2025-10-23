ba8fa4284fb26c91ba0758c4f6b83b4906e67cff
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2025-10-23 10:04:48 -0600

    Metodo Buscar

--- src/views/Categorias.jsx
+++ src/views/Categorias.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import TablaCategoria from "../components/categorias/TablaCategoria";
+import CuadroBusquedas from "../components/busquedas/CuadroBusquedas";
 
 const Categorias = () => {
   const [categorias, setCategorias] = useState([]);
   const [cargando, setCargando] = useState(true);
+  const [categoriasFiltradas, setCategoriasFiltradas] = useState([]);
+  const [textoBusqueda, setTextoBusqueda] = useState("");
 
   const obtenerCategorias = async () => {
     try {
@@ -12,14 +15,26 @@ const Categorias = () => {
       if (!respuesta.ok) {
         throw new Error("Error al obtener las categorias");
       }
-
       const datos = await respuesta.json();
       setCategorias(datos);
+      setCategoriasFiltradas(datos);
       setCargando(false);
     } catch (error) {
       console.error(error.message);
       setCargando(false);
     }
+  };
+
+  const manejarCambioBusqueda = (e) => {
+    const texto = e.target.value.toLowerCase();
+    setTextoBusqueda(texto);
+
+    const filtradas = categorias.filter(
+      (categoria) =>
+        categoria.nombre_categoria.toLowerCase().includes(texto) ||
+        categoria.descripcion_categoria.toLowerCase().includes(texto)
+    );
+    setCategoriasFiltradas(filtradas);
   };
 
   useEffect(() => {
@@ -28,13 +43,21 @@ const Categorias = () => {
 
   return (
     <>
-      <Container className = "mt-4">
+      <Container className="mt-4">
         <h4> Categorias </h4>
-        <TablaCategoria categorias={categorias}
-        cargando={cargando}/>
+        <Row>
+          <Col lg={5} md={8} sm={8} xs={7}>
+            <CuadroBusquedas
+              textoBusqueda={textoBusqueda}
+              manejarCambioBusqueda={manejarCambioBusqueda}
+            />
+          </Col>
+        </Row>
+
+        <TablaCategoria categorias={categoriasFiltradas} cargando={cargando} />
       </Container>
     </>
   );
-}
+};
 
 export default Categorias;
